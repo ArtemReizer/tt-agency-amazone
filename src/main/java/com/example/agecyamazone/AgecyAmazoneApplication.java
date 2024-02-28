7ed210869e03d11aa6cc72017cc0c4522cecb63f
Artem Reizer <artem1707200290@gmail.com>
2024-02-28 14:35:21 +0200

    first commit

---
+++ src/main/java/com/example/agecyamazone/AgecyAmazoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.agecyamazone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgecyAmazoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgecyAmazoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/agecyamazone/AgecyAmazoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agecyamazone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgecyAmazoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
